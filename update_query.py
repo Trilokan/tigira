a635734d36f80eaa48a19f57c88cd1f4aa2f7eb7
rameshkumar <rameshkumar@ajaxmediatech.com>
2019-01-07 12:16:08 +0530

    compl

--- update_query.py
+++ update_query.py
@@ -2,10 +2,10 @@ from table_list import DataMigration
 from t_name import tables
 
 
-database_old = "live_db"
-database_new = "volk"
-user = "ramesh"
-password = "ramesh"
+database_old = "version_7_db"
+database_new = "version_11_db"
+user = "ubuntu"
+password = "vvti_ajax"
 host = "127.0.0.1"
 port = "5432"
 
