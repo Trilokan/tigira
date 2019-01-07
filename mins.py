275ea244b7adbdc65ffb3ae48ab3286519b8ae5d
rameshkumar <rameshkumar@ajaxmediatech.com>
2019-01-07 11:46:41 +0530

    compl

--- mins.py
+++ mins.py
@@ -2,8 +2,8 @@ from table_list import DataMigration
 from t_name import tables
 
 
-database_old = "live_db"
-database_new = "volk"
+database_old = "version_7_db"
+database_new = "version_11_db"
 user = "ramesh"
 password = "ramesh"
 host = "127.0.0.1"
