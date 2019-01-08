9d842db94a216ae6500a2e1a1c09602fb069fdf9
rameshkumar <rameshkumar@ajaxmediatech.com>
2019-01-08 14:12:05 +0530

    compl

--- migrate_data/main.py
+++ migrate_data/main.py
@@ -1,7 +1,7 @@
-from table_list import DataMigration
+from migrate import DataMigration
 # from t_name import tables
 
-from l_name import tables
+from table_list_direct import tables
 
 # database_old = "version_7_db"
 # database_new = "version_11_db"
@@ -12,8 +12,6 @@ database_old = "live_db"
 database_new = "volk"
 user = "ramesh"
 password = "ramesh"
-
-
 
 host = "127.0.0.1"
 port = "5432"
@@ -31,9 +29,9 @@ for table_name in tables:
         rows = live.get_row_vals(col_list, table_name)
 
         new.update_query(table_name, col_list, rows)
-        print table_name
+
     except:
-        pass
+        print table_name
 
     new.close()
 
---
+++ migrate_ids/main.py
@@ -0,0 +1,42 @@
+from migrate import DataMigration
+from table_array import tables
+
+
+database_old = "version_7_db"
+database_new = "version_11_db"
+user = "ubuntu"
+password = "vvti_ajax"
+
+# database_old = "live_db"
+# database_new = "volk"
+# user = "ramesh"
+# password = "ramesh"
+
+host = "127.0.0.1"
+port = "5432"
+
+# table_name = "account_account"
+
+
+for table_name in tables:
+    live = DataMigration(database_old, user, password, host, port)
+    new = DataMigration(database_new, user, password, host, port)
+    live.authentication()
+    new.authentication()
+
+    try:
+        # Remove NOT NULL Constraints
+        new.remove_not_null(table_name)
+
+        # GET id list in new db
+        ids = new.get_updated_ids_list(table_name)
+
+        # Update all id in new db
+        new.data_copy(database_old, database_new, table_name, ids)
+
+    except:
+        new.table_transfer(database_old, database_new, table_name)
+
+    new.close()
+
+
