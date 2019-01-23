04f040d3598d6ecff2ddf594faca23dca788a43c
rameshkumar <rameshkumar@ajaxmediatech.com>
2019-01-23 12:29:38 +0530

    compl

--- advance_migrate/single.py
+++ advance_migrate/single.py
@@ -19,19 +19,25 @@ table_name = sys.argv[1]
 ext_from = sys.argv[2]
 ext_till = sys.argv[3]
 
-live = DataMigration(database_old, user, password, host, port)
-new = DataMigration(database_new, user, password, host, port)
-live.authentication()
-new.authentication()
+for i in range(0, 100):
+    for j in range(0, 100):
+        for z in range(0, 100):
+            ext_from = ext_from + 100
+            ext_till = ext_till + 100
 
-col_list = new.get_col_names(table_name)
+            live = DataMigration(database_old, user, password, host, port)
+            new = DataMigration(database_new, user, password, host, port)
+            live.authentication()
+            new.authentication()
 
-for col in col_list:
-    col_status = live.check_col_in_db(col, table_name)
-    if col_status:
-        rows = live.get_col_vals(col, table_name, ext_from, ext_till)
-        new.advance_update_query(table_name, col, rows)
+            col_list = new.get_col_names(table_name)
 
-new.close()
+            for col in col_list:
+                col_status = live.check_col_in_db(col, table_name)
+                if col_status:
+                    rows = live.get_col_vals(col, table_name, ext_from, ext_till)
+                    new.advance_update_query(table_name, col, rows)
+
+            new.close()
 
 
