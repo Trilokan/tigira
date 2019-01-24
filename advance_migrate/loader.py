ac1785500fce9bd05fb4706889fe128e54835733
rameshkumar <rameshkumar@ajaxmediatech.com>
2019-01-24 10:45:55 +0530

    compl

--- advance_migrate/loader.py
+++ advance_migrate/loader.py
@@ -1,27 +1,5 @@
 import os
 import time
-import psutil
-
-def findProcessIdByName(processName):
-    '''
-    Get a list of all the PIDs of a all the running process whose name contains
-    the given string processName
-    '''
-
-    listOfProcessObjects = []
-
-    # Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
-            # Check if process name contains the given name string.
-            if processName.lower() in pinfo['name'].lower():
-                listOfProcessObjects.append(pinfo)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-
-    return listOfProcessObjects;
-
 
 start = 0
 end = 100
@@ -31,6 +9,4 @@ for i in range(1, 1000):
     os.system(cmd)
     start = start + 100
     end = end + 100
-    listOfProcessIds = findProcessIdByName('main')
-    if listOfProcessIds > 2:
-        time.sleep(500)
+    time.sleep(500)
