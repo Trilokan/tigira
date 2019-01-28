1094c952551c5e15b737637df6b91f1ca933a70e
rameshkumar <rameshkumar@ajaxmediatech.com>
2019-01-28 11:19:10 +0530

    compl

--- advance_migrate/loader.py
+++ advance_migrate/loader.py
@@ -1,12 +1,12 @@
 import os
 import time
 
-start = 0
-end = 1000
+start = 50000
+end = 51000
 
-for i in range(1, 100):
+for i in range(1, 200):
     cmd = "nohup python /home/ubuntu/sel_1/tigira/advance_migrate/main.py {0} {1} &".format(start, end)
     os.system(cmd)
     start = start + 1000
     end = end + 1000
-    time.sleep(1000)
+    time.sleep(200)
